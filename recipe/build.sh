d32c315dba185adc2ce103781673faa1d46895b4
Riccardo Vianello <riccardo.vianello@gmail.com>
2025-03-01 22:56:04 +0100

    set cmake option CHEMICALITE_ENABLE_OPTIMIZED_POPCNT

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,11 +9,19 @@ if [[ "$target_platform" == osx-arm64 ]] || [[ "$target_platform" == osx-64 ]]; 
     export CXXFLAGS="-D_LIBCPP_DISABLE_AVAILABILITY -D_HAS_AUTO_PTR_ETC=0 $CXXFLAGS"
 fi
 
+POPCNT_OPTIMIZATION="ON"
+if [[ "$target_platform" == osx-arm64 ]] || [[ "$target_platform" == linux-ppc64le ]] || [[ "$target_platform" == linux-aarch64 ]]; then
+    # PowerPC includes -mcpu=power8 optimizations already
+    # ARM does not have popcnt instructions, afaik
+    POPCNT_OPTIMIZATION="OFF"
+fi
+
 cmake ${CMAKE_ARGS} \
     -D CMAKE_INSTALL_PREFIX=$PREFIX \
     -D CMAKE_INSTALL_LIBDIR=lib \
     -D CMAKE_BUILD_TYPE=Release \
     -D CMAKE_CATCH_DISCOVER_TESTS_DISCOVERY_MODE=PRE_TEST \
+    -D CHEMICALITE_ENABLE_OPTIMIZED_POPCNT="${POPCNT_OPTIMIZATION}" \
     $SRC_DIR
 
 make
